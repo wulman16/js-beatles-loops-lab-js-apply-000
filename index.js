811ed385468a2ab914158647876a84964405d0d3
William Ulman <will.ulman@gmail.com>
2018-12-28 20:32:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     var sentence = musicians[i] + ' plays ' + instruments[i];
     arr.push(sentence);
   }
