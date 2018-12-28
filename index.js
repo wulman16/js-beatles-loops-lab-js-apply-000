ce77fc5b0eded24450ee7338dcb945dfa8943904
William Ulman <will.ulman@gmail.com>
2018-12-28 20:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (i=0; i < musicians.length; i++) {
-    arr += `${musicians[i]} plays ${instruments[i]}`;
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return arr;
 }
